b1f804e17d609bc79ec4da65295ce6eda82ca721
DanDev_ <danuart21@gmail.com>
2024-04-06 23:13:34 +0800

    learn web scrapping with vanilla js

---
+++ index.js
@@ -0,0 +1,26 @@
+import puppeteer from 'puppeteer'
+
+const url = "https://primakara.ac.id/blog"
+
+const main = async() => {   
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url)
+    
+    const allArticles = await page.evaluate(() => {
+        const articles = document.querySelectorAll(".headline_small_secondary")
+
+        return Array.from(articles).map((article) => {
+            const category = article.querySelector(".headline_small_secondary__details .headline_small_secondary__details__category").innerText
+            const title = article.querySelector(".headline_small_secondary__details .headline_small_secondary__details__paragraph").innerText
+            return {
+                category,
+                title,
+            }
+        })
+    })
+
+    console.log(allArticles)
+}
+
+main()
